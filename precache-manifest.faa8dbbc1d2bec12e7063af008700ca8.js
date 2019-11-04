04925b3937545ad31e9f45e189e15effb41136e2
SG <sgng1@e.ntu.edu.sg>
2019-11-04 04:27:21 +0000

    Updates

---
+++ precache-manifest.faa8dbbc1d2bec12e7063af008700ca8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08549d7d5412894f12ddabe944dbe254",
+    "url": "/gh-pages-cra/index.html"
+  },
+  {
+    "revision": "949a8802c8267b691a1a",
+    "url": "/gh-pages-cra/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "7e3b5d5a6ce0c4a3c919",
+    "url": "/gh-pages-cra/static/js/2.3a0df56c.chunk.js"
+  },
+  {
+    "revision": "949a8802c8267b691a1a",
+    "url": "/gh-pages-cra/static/js/main.0da6e6ed.chunk.js"
+  },
+  {
+    "revision": "cd7d658f1ebfcbd8f916",
+    "url": "/gh-pages-cra/static/js/runtime-main.bd46320f.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/gh-pages-cra/static/media/logo.25bf045c.svg"
+  }
+]);
